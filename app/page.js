79daa160f9821c787146801b4bd274dfef003af8
Jaredg01 <jared.gutierrez@edu.sait.ca>
2025-05-20 13:44:52 -0600

    added week 3 assignment
    - created a shopping list
    - used components to make items and list

--- app/page.js
+++ app/page.js
@@ -5,9 +5,18 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link 
-      href="./week-2"
-      className="text-cyan-600 underline hover:text-cyan-300">Week 2</Link>
+      <ul>
+        <li>
+          <Link 
+          href="./week-2"
+          className="text-cyan-600 underline hover:text-cyan-300">Week 2</Link>
+        </li>
+        <li>
+          <Link 
+          href="./week-3"
+          className="text-cyan-600 underline hover:text-cyan-300">Week 3</Link>
+        </li>
+      </ul>
     </main>
   );
 }
