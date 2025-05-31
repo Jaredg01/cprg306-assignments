e860a0eb1061e4b16d7918c217aef1a6eaa64b29
Jaredg01 <jared.gutierrez.jg@gmail.com>
2025-05-30 22:22:59 -0600

    Week 4
    Added  a week 4 folder
    created a new-item function
    created a page for new-item function

--- app/page.js
+++ app/page.js
@@ -16,6 +16,11 @@ export default function Home() {
           href="./week-3"
           className="text-cyan-600 underline hover:text-cyan-300">Week 3</Link>
         </li>
+        <li>
+          <Link 
+          href="./week-4"
+          className="text-cyan-600 underline hover:text-cyan-300">Week 4</Link>
+        </li>
       </ul>
     </main>
   );
