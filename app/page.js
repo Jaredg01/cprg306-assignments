056e93c501400ca347b2e6d4585576f0c356a6fe
Jaredg01 <jared.gutierrez.jg@gmail.com>
2025-06-05 11:49:36 -0600

    finished week-5
    added a form card that takes in a name, category, and quantity then when added alert shows the added item
    
    added week-5 to main page, created a styles variable for week links

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Link from "next/link";
 
 
 export default function Home() {
+
+  let linkStyles = "text-cyan-600 underline hover:text-cyan-300"
+
   return (
     <main>
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
@@ -9,17 +12,22 @@ export default function Home() {
         <li>
           <Link 
           href="./week-2"
-          className="text-cyan-600 underline hover:text-cyan-300">Week 2</Link>
+          className={linkStyles}>Week 2</Link>
         </li>
         <li>
           <Link 
           href="./week-3"
-          className="text-cyan-600 underline hover:text-cyan-300">Week 3</Link>
+          className={linkStyles}>Week 3</Link>
         </li>
         <li>
           <Link 
           href="./week-4"
-          className="text-cyan-600 underline hover:text-cyan-300">Week 4</Link>
+          className={linkStyles}>Week 4</Link>
+        </li>
+        <li>
+          <Link 
+          href="./week-5"
+          className={linkStyles}>Week 5</Link>
         </li>
       </ul>
     </main>
